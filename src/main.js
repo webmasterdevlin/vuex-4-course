c658a0fde78899c6ba71239b2438be632ae080af
Devlin <webmasterdevlin@gmail.com>
2020-07-26 08:34:18 +0200

    updated

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue';
+import { createApp } from "vue";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
+
+/* Vue.js version 2
+
+import Vue from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+    render: h => h(App),
+}).$mount('#app')
+
+*/
