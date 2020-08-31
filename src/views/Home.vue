50976b41ebd1094face23fc0e7141d082af1467e
Devlin D <webmasterdevlin>
2020-09-01 00:27:59 +0200

    updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <h1>Vue 3, Vue Router 4, Vuex 4, and Bootstrap 5 😜</h1>
+    <h1>Vue 3, Vue Router 4, Vuex 4, VeeValidate 4, and Bootstrap 5 😜</h1>
   </div>
 </template>
 
