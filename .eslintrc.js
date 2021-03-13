3945bb222666cda8e53b24cbe34ffae0f6dcdd8c
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-03-13 18:32:46 +0100

    updated

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  plugins: ["testing-library", "jest-dom"],
+  extends: [
+    "plugin:testing-library/recommended",
+    "plugin:testing-library/vue",
+    "plugin:jest-dom/recommended",
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    "vue/no-mutating-props": "off",
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
